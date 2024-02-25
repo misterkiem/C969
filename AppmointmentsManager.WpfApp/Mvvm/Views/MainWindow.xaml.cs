b52bf2a80f265f1bdd65a13e49b7a86efe85df40
Kiem Bui <kiem.z.bui@hawkeye3d.com>
2024-02-25 11:29:08 -0600

    fix typo in WpfApp name, move MainWindow to Mvvm\Views

---
+++ AppmointmentsManager.WpfApp/Mvvm/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace AppointmentsManager.WpfApp.Mvvm.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
